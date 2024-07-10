b81bd7939d7079e85aa6528f1168b01a92d757b7
ericsmslvdr <ericsmsalvador@gmail.com>
2024-07-10 18:26:52 +0800

    Update not found page look

--- src/app/routes/not-found.jsx
+++ src/app/routes/not-found.jsx
@@ -2,12 +2,11 @@ import { Link } from "react-router-dom"
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>404 - Not Found</h1>
-            <br />
-            <p>Sorry, the page you are looking for does not exist.</p>
-            <Link to="/" replace>
-                <button>Go to Home</button>
+        <div className="bg-gray-800 h-screen grid place-content-center">
+            <h1 className="text-4xl text-center">404 - Not Found</h1>
+            <p className="text-2xl text-center mt-4">Sorry, the page you are looking for does not exist.</p>
+            <Link to="/" replace className="text-center">
+                <button className="mt-10 bg-gray-900 p-4 rounded-lg hover:bg-gray-950">Go to Home</button>
             </Link>
         </div>
     )
